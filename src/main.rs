21d960bbb1f24a195d6bc3370fe8cb6c724656ad
Simon Lalonde <simlalonde@hotmail.com>
2025-07-28 12:39:28 -0400

    Run simple tcp listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+mod tcp_ip;
+
+use tcp_ip::{SocketAddr, establish_tcp_conn};
+
 fn main() {
-    println!("Hello, world!");
+    match SocketAddr::from_env() {
+        Ok(socket) => {
+            if let Err(e) = establish_tcp_conn(&socket) {
+                eprintln!("❌ Connection error: {e}");
+            }
+        }
+        Err(e) => {
+            eprintln!("❌ Failed to parse socket address: {e}");
+        }
+    }
 }
